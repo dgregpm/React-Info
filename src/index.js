e5ddd26330569ddf66a52b963521faff30668c72
dgregpm <dgregpm@gmail.com>
2022-07-17 16:08:02 -0400

    added component

--- src/index.js
+++ src/index.js
@@ -21,11 +21,34 @@ function Page() {
 );
   }
 
+function PageHeader() {
+  return (
+    <div>
+    <header>
+      <nav>
+        <img src={logo} width="100px" alt="logo" />
+      </nav>
+    </header>
+    <h1>Reasons I'm excited to learn React</h1>
+    <ol>
+        <li>It's a popular library, so I'll be 
+        able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer
+        if I know React</li>
+    </ol>
+    <footer>
+      <p>© 2022 Dickson development. All rights reserved.</p>
+    </footer>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Page />
+    {/* <Page /> */}
+    <PageHeader />
   </React.StrictMode>
 );
 
