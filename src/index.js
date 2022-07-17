15b4eaaac1d8da2daf209022bd970ffd024a7e90
dgregpm <dgregpm@gmail.com>
2022-07-17 14:20:00 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import logo from './logo.svg'
 import reportWebVitals from './reportWebVitals';
+
+function Page() {
+  return (
+  <div>
+      <img src={logo} width="100px" alt="react logo"/>
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
+);
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Page />
   </React.StrictMode>
 );
 
